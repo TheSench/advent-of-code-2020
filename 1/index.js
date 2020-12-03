75aa46e1be096b2cfd337c08c4962e39988b7261
Jon Senchyna <TheSench@users.noreply.github.com>
2020-12-03 15:26:25 -0500

    Refactor

--- 1/index.js
+++ 1/index.js
@@ -2,28 +2,35 @@ const fs = require('fs');
 
 const TARGET_SUM = 2020;
 
+function getSortedExpenses(data) { 
+    return data.toString()
+        .split(/\r\n?/)
+        .map(Number)
+        .sort((a,b) => a - b);
+}
+
+function findExpensesWithSum(allExpenses, targetSum) {
+    let low = 0, high = allExpenses.length - 1;
+    while (low < high) {
+        const smallerExpense = allExpenses[low];
+        const largerExpense = allExpenses[high];
+        const sum = smallerExpense + largerExpense;
+        if (sum === targetSum) {
+            return [smallerExpense, largerExpense];
+        } else if (sum < targetSum) {
+            low++;
+        } else if (sum > targetSum) {
+            high--;
+        }
+    }
+}
+
 fs.readFile(__dirname + '/input.txt', function (err, data) {
     if (err) {
         throw err;
     }
-    const lines = data.toString()
-        .split(/\r\n?/)
-        .map(Number)
-        .sort((a,b) => a - b);
-    let low = 0, high = lines.length - 1;
-    while (low < high) {
-        const smallerNum = lines[low];
-        while (low < high) {
-            const largerNum = lines[high];
-            const sum = smallerNum + largerNum;
-            if (sum === TARGET_SUM) {
-                console.log(smallerNum, largerNum, smallerNum * largerNum);
-                return;
-            } else if (sum < TARGET_SUM) {
-                break;
-            }
-            high--;
-        }
-        low++;
-    }
+    const allExpenses = getSortedExpenses(data);
+    const expenses = findExpensesWithSum(allExpenses, TARGET_SUM);
+    const product = expenses[0] * expenses[1];
+    console.log(expenses, product);
 });
