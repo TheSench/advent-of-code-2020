ae76f596b1271a474752eff46ccb4e7ce890b0bf
Jon Senchyna <TheSench@users.noreply.github.com>
2020-12-04 22:46:15 -0500

    Solved day 3, part 1

---
+++ 3/part1.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+
+function getRows(data) {
+    return data.toString()
+        .split(/\r\n?/)
+        .map(line => [...line].map(char => char === '#'));
+}
+
+function getTrees(rows, dx, dy) {
+    let numTrees = 0;
+    let patternSize = rows[0].length;
+    for(let x = 0, y = 0; y < rows.length; y += dy) {
+        const row = rows[y];
+        if(row[x]) {
+            numTrees++;
+        }
+        x = (x + dx) % patternSize;
+    }
+    return numTrees;
+}
+
+fs.readFile(__dirname + '/input.txt', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    const allRows = getRows(data);
+    const numTrees = getTrees(allRows, 3, 1);
+    console.log(numTrees);
+});
