93a54df5f13f0191dd209ab84e675db3b97b3758
Jon Senchyna <TheSench@users.noreply.github.com>
2020-12-09 23:27:12 -0500

    Parse rules

--- 7/day7.js
+++ 7/day7.js
@@ -1,2 +1,29 @@
+const { readFile } = require('../utils/fileUtils');
+const { splitIntoLines } = require('../utils/stringSplitters');
+
+const RULE_REGEX = / contains? /
+function parseRule(line) {
+    const [type, contains] = line.split(/ bags? contains? /);
+    const contents = contains.split(', ')
+        .map(containedBag => containedBag.match(/(?<count>\d+) (?<type>\w+ \w+) bags?/))
+        .filter(match => match)
+        .map(({ groups }) => ({
+            count: Number(groups.count),
+            type: groups.type
+        }));
+
+    return {
+        type,
+        contents
+    };
+}
+
+function parseRules(text) {
+    return splitIntoLines(text)
+        .map(parseRule);
+}
+
 module.exports = {
+    parseRule,
+    parseRules
 };
--- 7/day7.test.js
+++ 7/day7.test.js
@@ -1,9 +1,56 @@
 const { describe, expect } = require('@jest/globals');
 const { outdent } = require('../utils/testUtils');
-const {  } = require('./day7');
+const { parseRule } = require('./day7');
 
-describe('day7', () => {
-    it('stub test', () => {
-        expect(true).toBe(true);
+describe('parseRule', () => {
+    it('parses a bag that contains no other bags', () => {
+        const rule = parseRule('faded blue bags contain no other bags.');
+
+        expect(rule).toEqual({
+            type: 'faded blue',
+            contents: []
+        });
+    });
+
+    it('parses a bag that contains one other type of bag', () => {
+        const rule = parseRule('bright white bags contain 1 shiny gold bag.');
+
+        expect(rule).toEqual({
+            type: 'bright white',
+            contents: [{
+                type: 'shiny gold',
+                count: 1
+            }]
+        });
+    });
+
+    it('parses a bag that contains multiple types of bag', () => {
+        const rule = parseRule('muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.');
+
+        expect(rule).toEqual({
+            type: 'muted yellow',
+            contents: [{
+                type: 'shiny gold',
+                count: 2
+            },{
+                type: 'faded blue',
+                count: 9
+            }]
+        });
+    });
+
+    it('handles plural and singular bags', () => {
+        const rule = parseRule('shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.');
+
+        expect(rule).toEqual({
+            type: 'shiny gold',
+            contents: [{
+                type: 'dark olive',
+                count: 1
+            },{
+                type: 'vibrant plum',
+                count: 2
+            }]
+        });
     });
 });
