8ebbfd8eedee465b3960a7651e70b7284c93bfe9
Jon Senchyna <TheSench@users.noreply.github.com>
2020-12-28 07:43:26 -0500

    Add countOccurrences

--- utils/reductions.js
+++ utils/reductions.js
@@ -7,6 +7,19 @@ function sum(a, b) {
     return a + b;
 }
 
+/**
+ * Count the number of occurrences of each element in the list.
+ * @param {Array} list 
+ */
+function countOccurrences(list) {
+    return list.reduce((map, next) => {
+        if (!map[next]) map[next] = 0;
+        map[next]++;
+        return map;
+    }, {});
+};
+
 module.exports = {
-    sum
+    sum,
+    countOccurrences
 };
--- utils/reductions.test.js
+++ utils/reductions.test.js
@@ -1,4 +1,4 @@
-const { sum } = require('./reductions');
+const { sum, countOccurrences } = require('./reductions');
 
 describe('sum', () => {
     it.each([
@@ -12,5 +12,49 @@ describe('sum', () => {
     it('ignores index when used as a reduction', () => {
         const total = [1, 1, 1, 1].reduce(sum);
         expect(total).toBe(4);
+    });
+});
+
+describe('countOccurrences', () => {
+    it('returns 1 for each unique number', () => {
+        const numbers = [1, 2, 3, 4, 5];
+
+        const occurrences = countOccurrences(numbers);
+
+        expect(occurrences).toEqual({
+            1: 1,
+            2: 1,
+            3: 1,
+            4: 1,
+            5: 1
+        });
+    });
+
+    it('counts duplicates', () => {
+        const numbers = [1, 1, 2, 3, 4, 4, 5];
+
+        const occurrences = countOccurrences(numbers);
+
+        expect(occurrences).toEqual({
+            1: 2,
+            2: 1,
+            3: 1,
+            4: 2,
+            5: 1
+        });
+    });
+
+    it('accepts strings', () => {
+        const numbers = ['a', 'a', 'b', 'c', 'd', 'e'];
+
+        const occurrences = countOccurrences(numbers);
+
+        expect(occurrences).toEqual({
+            a: 2,
+            b: 1,
+            c: 1,
+            d: 1,
+            e: 1
+        });
     });
 });
